fb8360fcce2559a4f2f93ab9afb0fc6cb9f7751b
Otávio Fernandes <oaugustus@gmail.com>
2018-07-24 19:11:09 -0300

    Alterado código de injeção de recursos

--- src/Injector/Injector.php
+++ src/Injector/Injector.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Injector;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use JShrink\Minifier;
+use Less_Parser;
 
 class Injector
 {
@@ -14,106 +16,129 @@ class Injector
     protected $compile;
     protected $minify;
     protected $scripts = array();
-
     private $moduleList = array();
 
     /**
-     * Inicializa o loader de scripts.
+     * Injector constructor.
      *
-     * @param $sourceDir
-     * @param $webDir
+     * @param string $webDir    Diretório web da aplicação
+     * @param string $deployDir Diretório onde serão gerados os arquivos de deploy
+     * @param array  $sources   Chave das fontes de extração de assets
+     * @param bool   $build     Se deverá ser gerado um único arquivo de build para cada fonte de extração
+     * @param bool   $minify    Se o arquivo de build deverá ser minificado.
      */
-    public function __construct($sourceDir, $webDir, $deployDir, $defs, $compile = false, $minify = false)
+    public function __construct($webDir, $deployDir, $sources, $build = false, $minify = false)
     {
-        $this->sourceDir = $sourceDir;
         $this->webDir = $webDir;
         $this->deployDir = $deployDir;
-        $this->defs = $defs;
-        $this->compile = $compile;
+        $this->defs = $sources;
+        $this->compile = $build;
         $this->minify = $minify;
+        $this->checkDeployDir();
     }
 
     /**
      * Carrega um módulo de scripts.
      *
-     * @param string  $module
-     * @param string  $type
+     * @param string $module
+     * @param string $type
+     * @param bool   $compile
      *
      * @return string
      *
      * @throws \Exception
      */
-    public function inject($module, $type = 'js', $compile = true)
+    public function inject($module, $type = 'js', $compile = false)
     {
-        $paramKey = 'inject.'.$module;
         $this->moduleList = array();
-
-        if (!isset($this->defs[$paramKey])) {
-            throw new \Exception('O módulo '.$module.' não foi definido nas configurações. O parâmetro "inject.'.$module.' não foi localizado!"');
-        } else {
-            return $this->injectResource($paramKey, $type, $compile);
+        if ($this->compile) {
+            $compile = true;
         }
-
+        if (!isset($this->defs['inject.'.$module])) {
+            throw new \Exception(
+                sprintf(
+                    'O módulo %s não foi definido nas configurações. Parâmetro "inject.%s" não localizado!', $module
+                )
+            );
+        } else {
+            return $this->injectResource($module, $type, $compile);
+        }
     }
 
     /**
      * Gera os scripts dos módulos de acordo com a definição de configuração.
      *
-     * @param string  $paramKey
-     * @param string  $type
-     * @param boolean $compile
+     * @param string $module
+     * @param string $type
+     * @param bool   $compile
      *
      * @return string
      */
-    protected function injectResource($paramKey, $type, $compile)
+    protected function injectResource($module, $type, $compile)
     {
-        $key = explode('.',$paramKey);
-        $buildFileName = end($key).".build.".$type;
-        $buildFileFullname = $this->deployDir."/".$buildFileName;
-
-        if ($this->compile && file_exists($buildFileFullname)) {
-            print($this->createIncludeTag("./".$this->deployDir."/".$buildFileName, $type));
+        $ext = $this->getResourceExtension($type);
+        $buildFileName = $module.'.build.'.$ext;
+        $buildFileFullname = $this->deployDir.'/'.$buildFileName;
+        $path = '.'.str_replace($this->webDir, '', $this->deployDir).'/';
+        // se está em estado de compilação e o arquivo compilado existe
+        if ($compile && file_exists($buildFileFullname)) {
+            if ($type == 'less') {
+                $type = 'css';
+            }
+            echo $this->createIncludeTag($path.$buildFileName, $type);
         } else {
-
-            $scripts = $this->buildResourceList($paramKey, $type);
-
-            $include = '';
-
-            foreach ($scripts as $module => $list) {
-
-                foreach ($list as $script) {
-
-                    if ($this->compile && $compile) {
-                        @$include.= "\n".file_get_contents($this->webDir."/".$script)."\n";
-                    } else {
-                        $include.= $this->createIncludeTag($script, $type);
-                    }
-                }
-            }
-
-            if ($this->compile && $compile) {
-                if ($this->minify) {
-                    file_put_contents($this->deployDir."/".$buildFileName,Minifier::minify($include,array('flaggedComments' => false)));
+            // recupera a lista de recursos
+            $scripts = $this->buildResourceList($module, $type);
+            // concatena os recursos para inclusão no template
+            $resource = $this->concatResources($scripts, $type, $compile);
+            if ($compile) { // se é para compilar
+                // salva  o arquivo da compilação do recurso
+                if ($this->minify && $type == 'js') {
+                    file_put_contents($buildFileFullname, Minifier::minify($resource, array('flaggedComments' => false)));
                 } else {
-                    file_put_contents($this->deployDir."/".$buildFileName, $include);
+                    file_put_contents($buildFileFullname, $resource);
                 }
-
-
-                print($this->createIncludeTag("./".$this->deployDir."/".$buildFileName, $type));
+                // escreve o include da compilação
+                echo $this->createIncludeTag($path.$buildFileName, $type);
             } else {
-                echo $include;
+                // escreve as tags de inclusão dos recursos não compilados
+                echo $resource;
             }
+        }
+    }
 
-
+    /**
+     * Concatena os recursos para criação do script de build ou tags de inclusão.
+     *
+     * @param array  $resources
+     * @param string $type
+     * @param bool   $compile
+     *
+     * @return string
+     */
+    private function concatResources($resources, $type, $compile)
+    {
+        $concat = '';
+        foreach ($resources as $script) {
+            if ($compile) {
+                if ($type == 'less') {
+                    @$concat .= "\n".$this->parseLess($this->webDir.'/'.$script)."\n";
+                } else {
+                    @$concat .= "\n".file_get_contents($this->webDir.'/'.$script)."\n";
+                }
+            } else {
+                $concat .= $this->createIncludeTag($script, $type);
+            }
         }
 
+        return $concat;
     }
 
     /**
      * Cria a tag de inclusão do recurso de acordo com o seu tipo.
      *
      * @param string $file
-     * @param strine $type
+     * @param string $type
      *
      * @return string
      */
@@ -121,10 +146,27 @@ class Injector
     {
         switch ($type) {
             case 'js':
-                return sprintf("<script type='text/javascript' src='%s'></script>\n",$file);
+                return sprintf("<script type='text/javascript' src='%s'></script>\n", $file);
             case 'css':
                 return sprintf("<link rel='stylesheet' href='%s'>\n", $file);
+            case 'less':
+                return sprintf("<style>\n%s</style>", $this->parseLess($file));
         }
+    }
+
+    /**
+     * Faz o parse do arquivo less.
+     *
+     * @param string $file
+     *
+     * @return string Conteúdo parseado
+     */
+    protected function parseLess($file)
+    {
+        $parser = new Less_Parser();
+        $parser->parseFile($file);
+
+        return $parser->getCss();
     }
 
     /**
@@ -137,10 +179,8 @@ class Injector
      */
     protected function buildResourceList($module, $type)
     {
-        $path = $this->defs[$module];
-
+        $path = $this->defs['inject.'.$module];
         $list = $this->getResourceList($module, $path, $type);
-        $list = array('module' => $list);
 
         return $list;
     }
@@ -156,33 +196,55 @@ class Injector
     private function getResourceList($module, $path, $type)
     {
         $finder = new Finder();
-
+        if (is_file($path)) {
+            return [$path];
+        }
         $dirs = $finder->directories()->in($path)->sortByType();
-
-        $directories  = array(
-            $path
+        $directories = array(
+            $path,
         );
-
         foreach ($dirs as $dir) {
-            $directory = $path."/".$dir->getRelativePath()."/".$dir->getFilename();
-            $directory = str_replace('//','/', $directory);
-
+            $directory = $path.'/'.$dir->getRelativePath().'/'.$dir->getFilename();
+            $directory = str_replace('//', '/', $directory);
             $directories[] = $directory;
         }
-
         $list = array();
-
         foreach ($directories as $dir) {
             $finder = new Finder();
-
-            $files = $finder->files()->in($dir)->name('*.'.$type)->depth("== 0")->sortByName();
-
+            $files = $finder->files()->in($dir)->name('*.'.$type)->depth('== 0')->sortByName();
             foreach ($files as $script) {
-                $list[] = $dir.$script->getRelativePath()."/".$script->getFilename();
+                $list[] = $dir.$script->getRelativePath().'/'.$script->getFilename();
             }
         }
 
-
         return $list;
+    }
+
+    /**
+     * Retorna a extensão de arquivo para compilação de um tipo de recurso.
+     *
+     * @param string $type
+     *
+     * @return string
+     */
+    private function getResourceExtension($type)
+    {
+        $ext = $type;
+        if ($ext == 'less') {
+            $ext = 'css';
+        }
+
+        return $ext;
+    }
+
+    /**
+     * Verifica se o diretório de deploy existe, e, caso não exista, força sua criação.
+     */
+    private function checkDeployDir()
+    {
+        $fs = new Filesystem();
+        if (!$fs->exists($this->deployDir)) {
+            $fs->mkdir($this->deployDir);
+        }
     }
 }
--- src/Injector/Provider/InjectorServiceProvider.php
+++ src/Injector/Provider/InjectorServiceProvider.php
@@ -1,44 +1,63 @@
 <?php
-namespace Injector\Provider;
 
+namespace Injector\Provider;
 
 use Injector\Injector;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+/**
+ * Class InjectorServiceProvider.
+ *
+ * Provedor de serviços do injetor de assets na front-view.
+ */
 class InjectorServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * Executado no ato de registro do provedor de serviços.
+     *
+     * @param Application $app
+     */
     public function register(Application $app)
     {
-        $app['injector'] = $app->share(function() use ($app){
+        $app['injector'] = $app->share(function () use ($app) {
             $keys = $app->keys();
             $defs = array();
 
-            foreach ($keys as $key){
-                if (strpos($key,'inject.') !== false){
+            foreach ($keys as $key) {
+                if (strpos($key, 'inject.') !== false) {
                     $defs[$key] = $app[$key];
                 }
             }
 
-            if (!$app['src_dir']) {
-                throw new \Exception('Não foi definido o diretório de scripts da aplicação ($app["src_dir"])!');
-            }
-
             if (!$app['web_dir']) {
-                throw new \Exception('Não foi definido o diretório web da aplicação ($app["web_dir"])!');
+                throw new \Exception(
+                    'Não foi definido o diretório web da aplicação ($app["web_dir"])!'
+                );
             }
 
             if (!$app['deploy_dir']) {
-                throw new \Exception('Não foi definido o caminho da geração dos builds da aplicação ($app["deploy_dir"])!');
+                throw new \Exception(
+                    'Não foi definido o caminho da geração dos builds da aplicação ($app["deploy_dir"])!'
+                );
             }
 
-            return new Injector($app['src_dir'], $app['web_dir'], $app['deploy_dir'], $defs, $app['injector.compile'], $app['injector.minify']);
+            return new Injector(
+                $app['web_dir'],
+                $app['deploy_dir'],
+                $defs,
+                $app['injector.compile'],
+                $app['injector.minify']
+            );
         });
     }
 
+    /**
+     * Função de boot do provedor.
+     *
+     * @param Application $app
+     */
     public function boot(Application $app)
     {
-
     }
-
 }
